466619d7057c17054803e848ec26c6453ee256f6
hadar kabeli <73433650+HKABELI@users.noreply.github.com>
2021-06-27 12:15:56 +0300

    Update

---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+/* eslint-disable */
+<template>
+    <div class="block" :class="{play:playing , pause:!playing}">
+            <img class="icon" :src='photo' alt="">
+    </div>
+</template>
+
+<script>
+
+    export default {
+        //data of a component is it's attrivutes (the data it owns)
+        props: {
+            name: String,
+            file: Object,
+            playing: Boolean,
+            photo: String,
+        }, 
+    }
+</script>
+
+<style scoped>
+
+.block {
+    margin: 10px;
+    border-radius: 10px;
+    padding: 50px;
+    color: white;
+}
+.block:active{
+    transform: scale(0.85);
+}
+
+.block:hover{
+    opacity: 0.8;
+    cursor: pointer;
+}
+.play{
+    background-color: rgb(111, 226, 111)
+}
+.pause{
+    background-color: rgb(226, 111, 111)
+}
+.icon{
+    height: 50px;
+    width: 50px;
+}
+
+
+</style>
